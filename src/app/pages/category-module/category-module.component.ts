d97e75247924b1327d5f0c17e19806ae6a2d1370
Palacios <martin.ibpa@outlook.com>
2020-08-04 20:34:26 -0500

    Se agregaron las funcionalidades de buscar, exportar pdf y excel

--- src/app/pages/category-module/category-module.component.ts
+++ src/app/pages/category-module/category-module.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 import { CategoryModel } from 'src/app/models/category-model';
+import { ExportDataService } from '../../services/export-to-excel.service';
+import { PdfServiceService } from '../../services/export-to-pdf-service.service';
 
 
 @Component({
@@ -12,22 +14,32 @@ export class CategoryModuleComponent implements OnInit {
  
   categoryStatus: CategoryModel = new CategoryModel();
   categories: CategoryModel [] = []
+  arrayCategoy = [];
+  title: string;
   searchText: any;
   idCategory: string;
   activo: Boolean = true;
   edit: boolean = true;
   refresh: boolean = false;
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService, private exportDataService: ExportDataService, private pdfServiceService: PdfServiceService) { }
 
   ngOnInit(): void {
     this.getCategory();
+    this.title = "Reporte de Categorias"
+    this.arrayCategoy = [];
   }
 
   getCategory(){
     this.categoryService.getCategory().then((res: any) => {
       this.categories = res.cnt;
-      console.log('todo bien!');
+      for(const i of this.categories){
+        let element = [
+          i.strName.replace(/\:null/gi,':""'),
+          i.strDescription.replace(/\:null/gi,':""')
+        ];
+        this.arrayCategoy.push(element);
+      }
     }).catch(err => {
       console.log(err)
     });
@@ -67,5 +79,58 @@ export class CategoryModuleComponent implements OnInit {
     if (this.refresh){
       this.ngOnInit();
     }
+  }
+
+  actexportPDF () {
+    this.exportPDF();
+    this.ngOnInit();
+  }
+
+  exportPDF() {
+    var data =[this.arrayCategoy];
+    data=[];
+     let header = [
+       {
+         text: "Nombre de la categoria",
+         style: "tableHeader",
+         bold: true,
+         fillColor: "#2a3e52",
+         color: "#ffffff",
+         size: 13,
+       },
+       {
+        text: "Descripción",
+        style: "tableHeader",
+        bold: true,
+        fillColor: "#2a3e52",
+        color: "#ffffff",
+        size: 13,
+      }
+     ]; 
+     this.pdfServiceService.generatePdf(
+       this.title,
+       header,
+       this.arrayCategoy,
+       "center",
+     );
+   }
+
+   exportAsXLSX() {
+    if (this.categories.length !== 0) {
+      let jsonobject = JSON.stringify(this.categories);
+      jsonobject = jsonobject.replace(/strName/gi, 'Nombre');
+      jsonobject = jsonobject.replace(/strDescription/gi, 'Descripción');
+      const jsonobject2 = JSON.parse(jsonobject);
+      const count = Object.keys(jsonobject2).length;
+      for (let i = 0; i < count; i++) {
+        delete jsonobject2[i].createdAt;
+        delete jsonobject2[i].updatedAt;
+        delete jsonobject2[i].blnStatus;
+        delete jsonobject2[i]._id;
+        delete jsonobject2[i].__v;
+      }
+      this.exportDataService.exportAsExcelFile(jsonobject2, `${this.title}`);
+    }
+
   }
 }
--- src/app/pages/cymbal-module/cymbal-module.component.ts
+++ src/app/pages/cymbal-module/cymbal-module.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CymbalService } from '../../services/cymbal.service';
 import { CymbalModel } from 'src/app/models/cymbal-model';
 import { ActivatedRoute } from '@angular/router';
+import { ExportDataService } from '../../services/export-to-excel.service';
+import { PdfServiceService } from '../../services/export-to-pdf-service.service';
 
 
 @Component({
@@ -14,26 +16,41 @@ export class CymbalModuleComponent implements OnInit {
   cymbalStatus: CymbalModel = new CymbalModel();
   cymbalArray: CymbalModel [] = [];
   saucers: CymbalModel [] = [];
+  arrayCymbal = [];
   searchText: any;
   idCategory: string; 
   idCymbal: string;
+  title: string;
   refresh: boolean = false;
   activo: Boolean = true;
   edit: boolean = true;
   
 
-  constructor(private cymbalService: CymbalService, private activatedRoute: ActivatedRoute) { }
+  constructor(private cymbalService: CymbalService, private activatedRoute: ActivatedRoute, private exportDataService: ExportDataService, private pdfServiceService: PdfServiceService) { }
 
   ngOnInit(): void {
   this.idCategory = this.activatedRoute.snapshot.params.id;
+  this.title = "Reporte de Platillos"
   this.getCymbal(this.idCategory);
+  this.arrayCymbal = [];
   }
 
   getCymbal(idCategory: string){
     this.cymbalService.getCymbalByCategory(idCategory).then((res: any) => {
       this.saucers = res.cnt
       console.log(res.cnt);
-      console.log('todo bien!');
+      for(const i of this.saucers){
+        let strPieces = i.nmbPieces.toString();
+        let strPrice = i.nmbPrice.toString();
+        let element = [
+          i.strName.replace(/\:null/gi,':""'),
+          i.strDescription.replace(/\:null/gi,':""'),
+          i.strIngredients.replace(/\:null/gi,':""'),
+          strPieces.replace(/\:null/gi,':""'),
+          strPrice.replace(/\:null/gi,':""')
+        ];
+        this.arrayCymbal.push(element);
+      }
     }).catch(err => {
       console.log(err);
     });
@@ -72,5 +89,87 @@ export class CymbalModuleComponent implements OnInit {
 
   editCanceled(e) {
     this.edit = e;
+  }
+
+  actexportPDF () {
+    this.exportPDF();
+    this.ngOnInit();
+  }
+
+  exportPDF() {
+    var data =[this.arrayCymbal];
+    data=[];
+     let header = [
+       {
+         text: "Platillo",
+         style: "tableHeader",
+         bold: true,
+         fillColor: "#2a3e52",
+         color: "#ffffff",
+         size: 13,
+       },
+       {
+        text: "Descripción",
+        style: "tableHeader",
+        bold: true,
+        fillColor: "#2a3e52",
+        color: "#ffffff",
+        size: 13,
+      },
+      {
+        text: "Ingredientes",
+        style: "tableHeader",
+        bold: true,
+        fillColor: "#2a3e52",
+        color: "#ffffff",
+        size: 13,
+      },
+      {
+        text: "Piezas",
+        style: "tableHeader",
+        bold: true,
+        fillColor: "#2a3e52",
+        color: "#ffffff",
+        size: 13,
+      },
+      {
+        text: "Precio",
+        style: "tableHeader",
+        bold: true,
+        fillColor: "#2a3e52",
+        color: "#ffffff",
+        size: 13,
+      }
+      
+     ]; 
+     this.pdfServiceService.generatePdf(
+       this.title,
+       header,
+       this.arrayCymbal,
+       "center",
+     );
+   }
+
+   exportAsXLSX() {
+    if (this.saucers.length !== 0) {
+      let jsonobject = JSON.stringify(this.saucers);
+      jsonobject = jsonobject.replace(/strName/gi, 'Nombre');
+      jsonobject = jsonobject.replace(/strDescription/gi, 'Descripción');
+      jsonobject = jsonobject.replace(/strIngredients/gi, 'Ingredientes');
+      jsonobject = jsonobject.replace(/nmbPieces/gi, 'Pieza');
+      jsonobject = jsonobject.replace(/nmbPrice/gi, 'Precio');
+      const jsonobject2 = JSON.parse(jsonobject);
+      const count = Object.keys(jsonobject2).length;
+      for (let i = 0; i < count; i++) {
+        delete jsonobject2[i].createdAt;
+        delete jsonobject2[i].updatedAt;
+        delete jsonobject2[i].blnStatus;
+        delete jsonobject2[i]._id;
+        delete jsonobject2[i].idCategory;
+        delete jsonobject2[i].__v;
+      }
+      this.exportDataService.exportAsExcelFile(jsonobject2, `${this.title}`);
+    }
+
   }
 }
